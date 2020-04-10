5fbaa3ced27f7b4d2796c96da9cb1b304ca412fa
Alena Kazarina <alenakazarina1@gmail.com>
2020-04-10 11:08:31 +0300

    refactored

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,60 +1,44 @@
 'use strict';
-const http = require(`http`);
+const express = require(`express`);
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 const {Commands, InputData, HttpCode, DataPath} = require(`../const`);
-
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!DOCTYPE html>
-    <html lang="ru">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Типотека</title>
-      </head>
-      <body>
-        ${message}
-      </body>
-    </html>
-  `.trim();
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-type': `text/html; charset=UTF-8`
-  });
-  res.end(template);
-};
-
-const onClientRequest = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(DataPath.OUT);
-        const articles = JSON.parse(fileContent);
-        const message = articles.map((article) => `<li>${article.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, `<p>Not Found</p>`);
-      }
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, `<p>Not Found</p>`);
-      break;
-  }
-};
 
 module.exports = {
   name: Commands.SERVER,
   run: (count) => {
     const port = parseInt(count, 10) || InputData.DEFAULT_PORT;
 
-    http.createServer(onClientRequest)
-      .listen(port)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(`Ошибка при создании сервера`, err);
+    const server = express();
+    server.use(express.json());
+
+    server.get(`/posts`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(DataPath.OUT);
+        if (fileContent.length) {
+          const mocks = JSON.parse(fileContent);
+          res.json(mocks);
+        } else {
+          res.json([]);
         }
-        return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      });
+      } catch (err) {
+        if (err.code === `ENOENT`) {
+          res.json([]);
+        } else {
+          res
+            .status(HttpCode.INTERNAL_SERVER_ERROR)
+            .send(err);
+        }
+      }
+    });
+
+    server.use((req, res) => {
+      res.status(HttpCode.NOT_FOUND)
+        .send(`Not Found`);
+    });
+
+    server.listen(port, () => {
+      console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    });
   }
 };
